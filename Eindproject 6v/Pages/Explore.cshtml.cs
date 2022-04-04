85ae9bd74d47bc12e0e38e469527542433a4a4c1
T. Systema <110443@st-maartenscollege.nl>
2022-04-04 20:02:27 +0200

    algemene layout gemaakt

---
+++ Eindproject 6v/Pages/Explore.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Eindproject_6v.Pages;
+
+public class ExploreModel : PageModel
+{
+    private readonly ILogger<ExploreModel> _logger;
+
+    public ExploreModel(ILogger<ExploreModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
