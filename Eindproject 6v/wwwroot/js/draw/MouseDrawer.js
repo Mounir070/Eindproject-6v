8e9d1d3dedb70b3352790d30192c6499b1d6b35e
Mounir Kalai <110560@st-maartenscollege.nl>
2022-04-05 01:45:39 +0200

    Eindelijk de tekenfunctie gemaakt

---
+++ Eindproject 6v/wwwroot/js/draw/MouseDrawer.js
@@ -0,0 +1,75 @@
+﻿import {Pencil} from "./Pencil.js";
+
+export class MouseDrawer {
+
+    /**
+     * @type {HTMLCanvasElement}
+     */
+    #canvas;
+    /**
+     * @type {CanvasRenderingContext2D}
+     */
+    #ctx;
+    #pencil;
+    #isDrawing = false;
+
+    constructor(canvas) {
+        this.#canvas = canvas;
+        this.#ctx = canvas.getContext("2d");
+        this.#pencil = new Pencil();
+    }
+
+    updatePencil(color) {
+        this.#pencil.setPencilColor(color);
+    }
+    
+    toggleEraser() {
+        this.#pencil.toggleEraser();
+    }
+    
+    isEraser() {
+        return this.#pencil.isEraser();
+    }
+
+    #draw() {
+        this.#ctx.beginPath();
+        this.#ctx.moveTo(this.#pencil.prevX, this.#pencil.prevY);
+        this.#ctx.lineTo(this.#pencil.currX, this.#pencil.currY);
+        this.#ctx.strokeStyle = this.#pencil.getPencilColor();
+        this.#ctx.lineWidth = this.#pencil.isEraser() ? Pencil.ERASER_RADIUS : Pencil.PENCIL_RADIUS;
+        this.#ctx.stroke();
+        this.#ctx.closePath();
+    }
+
+    onMouseDown(e) {
+        this.#pencil.prevX = this.#pencil.currX;
+        this.#pencil.prevY = this.#pencil.currY;
+        this.#pencil.currX = e.clientX - this.#canvas.getBoundingClientRect().left;
+        this.#pencil.currY = e.clientY - this.#canvas.getBoundingClientRect().top;
+
+        this.#isDrawing = true;
+        this.#ctx.beginPath();
+        this.#ctx.fillStyle = this.#pencil.getPencilColor();
+        this.#ctx.fillRect(this.#pencil.currX, this.#pencil.currY, Pencil.PENCIL_RADIUS, Pencil.PENCIL_RADIUS);
+        this.#ctx.closePath();
+    }
+
+    onMouseMove(e) {
+        if (!this.#isDrawing) {
+            return;
+        }
+        this.#pencil.prevX = this.#pencil.currX;
+        this.#pencil.prevY = this.#pencil.currY;
+        this.#pencil.currX = e.clientX - this.#canvas.getBoundingClientRect().left;
+        this.#pencil.currY = e.clientY - this.#canvas.getBoundingClientRect().top;
+        this.#draw();
+    }
+
+    onMouseUp(e) {
+        this.#isDrawing = false;
+    }
+
+    onMouseOut(e) {
+        this.#isDrawing = false;
+    }
+}
---
+++ Eindproject 6v/wwwroot/js/draw/Pencil.js
@@ -0,0 +1,33 @@
+﻿export class Pencil {
+
+    static PENCIL_RADIUS = 2;
+    static ERASER_RADIUS = 25;
+
+    #pencilColor = "black";
+    #isEraser = false;
+    prevX = 0;
+    currX = 0;
+    prevY = 0;
+    currY = 0;
+
+    getPencilColor() {
+        return this.#pencilColor;
+    }
+
+    setPencilColor(color) {
+        this.#pencilColor = color;
+    }
+
+    isEraser() {
+        return this.#isEraser;
+    }
+
+    toggleEraser() {
+        this.#isEraser = !this.#isEraser;
+        if (this.#isEraser) {
+            this.#pencilColor = "white";
+        } else {
+            this.#pencilColor = "transparent";
+        }
+    }
+}
