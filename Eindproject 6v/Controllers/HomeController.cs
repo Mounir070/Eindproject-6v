ffd53455c1724a4488a185d5054aab25dd02fc81
TimBrakeboer <110272@st-maartenscollege.nl>
2022-04-09 18:26:43 +0200

    Maak automatische database connection

--- Eindproject 6v/Controllers/HomeController.cs
+++ Eindproject 6v/Controllers/HomeController.cs
@@ -1,12 +1,44 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Eindproject_6v.Models;
+using MySql.Data.MySqlClient;
 
 namespace Eindproject_6v.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+
+    private const string ConnectionString = "Server=informatica.st-maartenscollege.nl;Database=110272;Uid=110272;Pwd=inf2021sql;";
+
+    private static List<ImageModel> GetRecentImages()
+    {
+        const string query = "select IMG_ID, IMG_TITLE, USER_NAME, IMG_DESCRIPTION, IMG_BLOB from img_info join user_info on img_info.IMG_AUTHOR_ID = user_info.USER_ID order by img_info.IMG_ID desc";
+        var images = new List<ImageModel>();
+        using var connection = new MySqlConnection(ConnectionString);
+        connection.Open();
+        var cmd = new MySqlCommand(query, connection);
+        using (var reader = cmd.ExecuteReader())
+        {
+            while (reader.Read())
+            {
+                var id = reader.GetInt32("IMG_ID");
+                
+                var title = reader.GetString("IMG_TITLE");
+                
+                var author = reader.GetString("USER_NAME");
+                
+                var description = reader.GetString("IMG_DESCRIPTION");
+
+                var size = reader.GetUInt32("IMG_SIZE");
+                var bytes = new byte[size];
+                reader.GetBytes(reader.GetOrdinal("IMG_BLOB"), 0, bytes, 0, (int) size);
+                images.Add(new ImageModel(id, title, author, description, bytes));
+            }
+        }
+        connection.Close();
+        return images;
+    }
 
     public HomeController(ILogger<HomeController> logger)
     {
@@ -21,7 +53,8 @@ public class HomeController : Controller
     [Route("Explore")]
     public IActionResult Explore()
     {
-        return View();
+        var images = GetRecentImages();
+        return View(images);
     }
 
     [Route("NotFound")]
---
+++ Eindproject 6v/Models/ImageModel.cs
@@ -0,0 +1,33 @@
+﻿namespace Eindproject_6v.Models;
+
+public class ImageModel
+{
+    public int id;
+    public string? title;
+    public string author;
+    public string? description;
+    public byte[] bytes;
+    public ImageModel(int id, string? title, string author, string? description, byte[] bytes)
+    {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.description = description;
+        this.bytes = bytes;
+    }
+    
+    public bool HasTitle()
+    {
+        return this.title is not null;
+    }
+
+    public bool HasDescription()
+    {
+        return this.description is not null;
+    }
+
+    public string GetBase64()
+    {
+        return Convert.ToBase64String(this.bytes);
+    }
+}
