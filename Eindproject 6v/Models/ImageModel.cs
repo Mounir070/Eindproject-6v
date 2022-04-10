c017ee3da09e83e3e5a8727e9c673374a6ce696a
Mounir070 <110560@st-maartenscollege.nl>
2022-04-10 21:39:59 +0200

    Versimpel code

--- Eindproject 6v/Models/ImageModel.cs
+++ Eindproject 6v/Models/ImageModel.cs
@@ -2,31 +2,22 @@
 
 namespace Eindproject_6v.Models;
 
-public class ImageModel
+// struct omdat de waardes niet null kunnen zijn
+public struct ImageModel
 {
     public int id;
-    public string? title;
+    public string title;
     public string author;
-    public string? description;
+    public string description;
     public byte[] bytes;
 
-    public ImageModel(int id, string? title, string author, string? description, byte[] bytes)
+    public ImageModel(int id, string title, string author, string description, byte[] bytes)
     {
         this.id = id;
         this.title = title;
         this.author = author;
         this.description = description;
         this.bytes = bytes;
-    }
-
-    public bool HasTitle()
-    {
-        return this.title is not null;
-    }
-
-    public bool HasDescription()
-    {
-        return this.description is not null;
     }
 
     public string GetBase64()
